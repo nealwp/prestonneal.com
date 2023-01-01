d3ab3867857ebe18e26e66ee007809a95d21f956
nealwp <mrprestonneal@gmail.com>
2023-01-01 11:13:07 -0800

    remove ts logo

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
 import { setupCounter } from './counter'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
