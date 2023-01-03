351c30fed0d8c2f6d26dbc7c9d9dc781d45eb923
nealwp <mrprestonneal@gmail.com>
2023-01-02 21:41:34 -0800

    add hedgehog

--- src/main-page.ts
+++ src/main-page.ts
@@ -7,6 +7,7 @@ const mainPage = {
 
     app.innerHTML = `
       <div class="m-auto text-center">
+        <img src='/hedgehog-noborder.svg' height=300 width=200>
         <h1 id="name" class="p-1">${mainPage.name}</h1>
         <h2 id="title" class="p-1">${mainPage.title}</h2>
       </div>
